2e59be7f94d5d0d7cb918a8279b7e6b53336622b
Gustavo Souza Gonçalves <gustavosouzagoncalves@gmail.com>
2013-06-02 15:56:06 -0300

    Update 0.0.0.1

--- Model/Mercado.cs
+++ Model/Mercado.cs
@@ -6,13 +6,44 @@ namespace TestProject1.Model
     public class Mercado : ModelBase
     {
         public String Nome { get; set; }
-
         public Int16 Logradouro { get; set; }
-
         public String Rua { get; set; }
-
         public String Bairro { get; set; }
-
-
+        public String Cidade { get; set; }
+        public String[] Estado
+        {
+            get
+            {
+                return new String[] { 
+                "AC",
+                "AL",
+                "AP",
+                "AM",
+                "BA",
+                "CE",
+                "DF",
+                "ES",
+                "GO",
+                "MA",
+                "MT",
+                "MS",
+                "MG",
+                "PA",
+                "PB",
+                "PR",
+                "PE",
+                "PI",
+                "RJ",
+                "RN",
+                "RS",
+                "RO",
+                "RR",
+                "SC",
+                "SP",
+                "SE",
+                "TO"};
+            }
+            set;
+        }
     }
 }
