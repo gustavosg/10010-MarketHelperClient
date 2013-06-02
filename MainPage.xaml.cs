3f31e27f31b993438237d7b837df958571047a4b
Gustavo Souza Gonçalves <gustavo@geraes.net>
2013-06-02 00:47:58 -0300

    Creating

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+
+namespace TestProject1
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPage_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SystemTray.IsVisible = false;
+
+        }
+    }
+}
---
+++ Tests/TestClass1.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.Tests
+{
+    [TestClass]
+    public class TestClass1 
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
